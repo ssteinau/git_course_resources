d1bcbfafad75af46b1f89eedc926c933f1d7d129
Sebastian Steinau <sebastian.steinau@steinautech.com>
2025-11-05 10:39:27 +0100

    removed subcategory from dimProduct

--- dimProduct.sql
+++ dimProduct.sql
@@ -2,6 +2,5 @@ CREATE TABLE dim.products (
     ProductID INT IDENTITY(1,1) PRIMARY KEY,
     ProductName NVARCHAR(255) NOT NULL,
     Category NVARCHAR(100), 
-    Subcategory NVARCHAR(100),
     Price DECIMAL(10,2) NOT NULL 
 );
